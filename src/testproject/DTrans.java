5e511a9e73e4d3ab4ae8ba22cc045b4840ecd7d1
Calvin Adhikang <66165193+calvinadhikang@users.noreply.github.com>
2021-11-21 22:25:57 +0700

    Add files via upload

--- src/testproject/DTrans.java
+++ src/testproject/DTrans.java
@@ -1,5 +1,9 @@
 
 package testproject; 
+
+import java.sql.PreparedStatement;
+import static testproject.DB.conn;
+
 public class DTrans extends Menu{
     private int quantity;
     private String keterangan;
@@ -26,4 +30,65 @@ public class DTrans extends Menu{
         this.keterangan = keterangan;
     }
     
+    public static void insertTableDTrans(String nomorNota, int idMenu, int qty, int harga, int subtotal) throws Exception {
+        if(nomorNota.equals("") || harga <= 0 || subtotal <= 0 ){
+            //exception
+            throw new Exception("Field tidak boleh kosong / harga > 0 !");
+        }
+        
+        try {
+            PreparedStatement ps = conn.prepareStatement
+                    ("insert into dtrans (nomornota, id_menu, qty, harga, subtotal) values (?, ?, ?, ?, ?)");
+            ps.setString(1, nomorNota);
+            ps.setInt(2, idMenu);
+            ps.setInt(3, qty);
+            ps.setInt(4, harga);
+            ps.setInt(5, subtotal);
+            
+            ps.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public static void updateTableDTrans(String nomorNota, int idMenu, int qty, int harga, int subtotal) throws Exception {
+        if(nomorNota.equals("") || harga <= 0 || subtotal <= 0 ){
+            //exception
+            throw new Exception("Field tidak boleh kosong / harga > 0 !");
+        }
+        
+        try {
+            PreparedStatement ps = conn.prepareStatement
+                    ("update dtrans set qty = ?, harga = ?, subtotal = ? where nomorNota = ? and id_menu = ?");
+            ps.setInt(1, qty);
+            ps.setInt(2, harga);
+            ps.setInt(3, subtotal);
+            ps.setString(4, nomorNota);
+            ps.setInt(5, idMenu);
+            
+            ps.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public static void deleteTableDTrans(String nomorNota, int idMenu) throws Exception {
+        int valid1 = -99;
+        valid1 = idMenu;
+        
+        if(valid1 == -99 || nomorNota.equals("")){
+            throw new Exception("Field tidak boleh kosong !");
+        }
+        
+        try {
+            PreparedStatement ps = conn.prepareStatement
+                        ("delete from dtrans where nomorNota = ? and id_menu = ?");
+            ps.setString(1, nomorNota);
+            ps.setInt(2, idMenu);
+
+            ps.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- src/testproject/Karyawan.java
+++ src/testproject/Karyawan.java
@@ -1,6 +1,9 @@
 
 package testproject;
  
+import java.sql.PreparedStatement;
+import static testproject.DB.conn;
+
 public class Karyawan {
     static String username;
     static int id;
@@ -21,4 +24,68 @@ public class Karyawan {
         Karyawan.id = id;
     }
     
+    public static void insertTableKaryawan(String username, String password, String nama, int status, int jabatan) throws Exception {
+        if(username.equals("") || password.equals("") || nama.equals("") || (!(status == 0 || status == 1)) || (!(jabatan == 1 || jabatan == 2)) ){
+            //exception
+            throw new Exception("Field tidak boleh kosong / harga > 0 !");
+        }
+        
+        try {
+            PreparedStatement ps = conn.prepareStatement
+                    ("insert into karyawan (username, password, nama, status, jabatan) values (?, ?, ?, ?, ?)");
+            ps.setString(1, username);
+            ps.setString(2, password);
+            ps.setString(3, nama);
+            ps.setInt(4, status);
+            ps.setInt(5, jabatan);
+            
+            ps.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public static void updateTableKaryawan(int idKaryawan, String username, String password, String nama, int status, int jabatan) throws Exception {
+        if(username.equals("") || password.equals("") || nama.equals("") || (!(status == 0 || status == 1)) || (!(jabatan == 1 || jabatan == 2)) ){
+            throw new Exception("Field tidak boleh kosong / harga harus > 0 !");
+        }
+        
+        try {
+            // TODO add your handling code here:
+            PreparedStatement ps = conn.prepareStatement
+                    ("update karyawan set username = ?, password = ?, nama = ?, status = ?, jabatan = ? where id_karyawan = ?");
+            
+            //
+            ps.setString(1, username);
+            ps.setString(2, password);
+            ps.setString(3, nama);
+            ps.setInt(4, status);
+            ps.setInt(5, jabatan);
+            ps.setInt(6, idKaryawan);
+            
+            ps.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public static void deleteTableKaryawan(int idKaryawan) throws Exception {
+        int valid = -99;
+        valid = idKaryawan;
+        
+        if(valid == -99){
+            throw new Exception("idKaryawan tidak boleh kosong !");
+        }
+        
+        try {
+            PreparedStatement pr = conn.prepareStatement
+                    ("delete from karyawan where id_karyawan = ?");
+            //
+            pr.setInt(1, idKaryawan);
+            
+            pr.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
